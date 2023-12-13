e950a60ecd4b08cf61175e1d5a2713ae30bd70e8
Caroline Lindström <33750059+CarolineLind@users.noreply.github.com>
2023-12-13 15:13:40 +0100

    Sista på formulären

--- nextjs-dashboard/app/ui/invoices/create-form.tsx
+++ nextjs-dashboard/app/ui/invoices/create-form.tsx
@@ -15,7 +15,7 @@ import { useFormState } from 'react-dom';
 export default function Form({ customers }: { customers: CustomerField[] }) {
   const initialState = { message: null, errors: {} };
   const [state, dispatch] = useFormState(createInvoice, initialState);
-  console.log(state);
+
   return (
     <form action={dispatch}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
@@ -136,13 +136,12 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
           </div>
         </fieldset>
         <div id="any-error" aria-live="polite" aria-atomic="true">
-         {state.errors?.status &&
-              state.errors.status.map((error: string) => (
-                <p className="mt-2 text-sm text-red-500" key={error}>
-                  {error}
-                </p>
-              ))
-            }
+         {state.message && (
+              <p className="mt-2 text-sm text-red-500" >
+                {state.message}
+              </p>
+            )
+          }
         </div>
       </div>
       <div className="mt-6 flex justify-end gap-4">
